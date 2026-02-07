88875d271aaac163bdd178b9564d5e8c053ba003
Aditya Kumar <adityakumar04315@gmail.com>
2026-02-07 16:50:25 +0530

    some new update of the game!!

--- new_state_machine/state_machine.gd
+++ new_state_machine/state_machine.gd
@@ -7,10 +7,15 @@ func _ready():
 	change_state("Patrol")
 	
 func _physics_process(delta):
+	if not owner or owner.is_dead:
+		set_physics_process(false)
+		return
 	if current_state:
 		current_state.update(delta)  # update the state
 		
 func change_state(state_name : String):
+	if owner.is_dead:
+		return
 	var new_state = get_node_or_null(state_name)
 	if new_state == null or new_state == current_state:
 		return
