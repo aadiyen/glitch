e645c23594e5410b1679bc32c58adcf7377526d0
Aditya Kumar <adityakumar04315@gmail.com>
2026-02-08 21:10:22 +0530

    long range enemy scene and script!!!
    coded enemy who can shooot at long range will code the player damage in next commit

--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -1,8 +1,8 @@
-extends AnimatedSprite2D
+extends Area2D
 
-@export var speed : int = 500
+
+@export var speed : int = 100
 @export var direction = Vector2.ZERO
-
 
 func _physics_process(delta):
 	position += direction * speed * delta
@@ -12,6 +12,8 @@ func _physics_process(delta):
 
 func _on_area_2d_body_entered(body: Node2D) -> void: # body signal trigger for  player
 	if body.is_in_group("player"):
+		print("player took bullet")
+		body.take_damage(10)
 		queue_free()
 	elif  body is TileMapLayer:
 		queue_free()
--- script/eggturrent.gd
+++ script/eggturrent.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 @export var enemy_shoot = preload("res://scenes/enemy_bullet.tscn")
+@export var shoot_direction = Vector2(-1,0)
 
 
 const GRAVITY : int = 1000
-
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var fire_timer: Timer = $FireTimer
 @onready var marker_2d: Marker2D = $Marker2D
 
@@ -12,7 +13,7 @@ var player = null
 var can_shoot = true
 func _physics_process(delta):
 	if !is_on_floor():
-		velocity.y += GRAVITY
+		velocity.y += GRAVITY * delta
 	move_and_slide()
 
 
@@ -20,12 +21,12 @@ func _physics_process(delta):
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body = player
+		player = body
 		fire_timer.start()
 		print("body entered attack!!")
 		
 
-		
+
 		
 		
 
@@ -37,13 +38,15 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 		print("timer stopped")
 
 
+func _on_fire_timer_timeout():
+	if player:
+		shoot()
+
 func shoot():
 	var enemy_shoot_instance = enemy_shoot.instantiate()
+	get_tree().root.add_child(enemy_shoot_instance)
 	enemy_shoot_instance.global_position = marker_2d.global_position
-	get_parent().add_child(enemy_shoot_instance)
 	
-	
-	
-	
-	
-	
+	#var dir = (player.global_position - global_position ).normalized()
+	enemy_shoot_instance.direction = shoot_direction
+	$AnimatedSprite2D.play("shoot")
